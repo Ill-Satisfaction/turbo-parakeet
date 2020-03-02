6cb0571f5a1d2d0e97197c0ea06a1809ca323d5e
akiah tullis <akiahtullis@u.boisestate.edu>
2020-03-01 20:02:56 -0500

    chapter 4 midway update

---
+++ toDoList/ToDoCD+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoCD+CoreDataClass.swift
+//  toDoList
+//
+//  Created by XCodeClub on 2020-03-01.
+//  Copyright © 2020 Akiah Tullis. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoCD)
+public class ToDoCD: NSManagedObject {
+
+}
---
+++ toDoList/ToDoCD+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoCD+CoreDataProperties.swift
+//  toDoList
+//
+//  Created by XCodeClub on 2020-03-01.
+//  Copyright © 2020 Akiah Tullis. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoCD {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoCD> {
+        return NSFetchRequest<ToDoCD>(entityName: "ToDoCD")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var priority: Int32
+
+}
