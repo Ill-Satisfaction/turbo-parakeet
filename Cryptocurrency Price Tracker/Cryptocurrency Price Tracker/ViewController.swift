f8319988fe816d72a17722aa7aa211027f43344b
akiah tullis <akiahtullis@u.boisestate.edu>
2020-03-03 00:58:49 -0500

    chap 6 midway update

--- Cryptocurrency Price Tracker/Cryptocurrency Price Tracker/ViewController.swift
+++ Cryptocurrency Price Tracker/Cryptocurrency Price Tracker/ViewController.swift
@@ -8,13 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var price: UILabel!
+    
+    var crpCcy: [String] = []
+    var ccy: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        crpCcy = ["BTC", "ETH", "XRP", "BCH"]
+        ccy = ["USD", "EUR", "JPY", "CHF"]
+        
+        self.picker.delegate = self
+        self.picker.dataSource = self
     }
 
-
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if (component == 0) {
+            return crpCcy.count
+        } else {
+            return ccy.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if (component == 0) {
+            return crpCcy[row]
+        } else {
+            return ccy[row]
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        getPrice(crpCcy: crpCcy[picker.selectedRow(inComponent: 0)], ccy: ccy[picker.selectedRow(inComponent: 1)])
+    }
+    
+    func getPrice(crpCcy: String, ccy: String) {
+        
+    }
+    
 }
 
